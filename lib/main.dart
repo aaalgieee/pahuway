31efa6f9c21999a1a8ec6d457d434f35268b1896
Al Gabriel <71442965+aaalgieee@users.noreply.github.com>
2021-09-09 19:39:05 +0800

    Formatting

--- lib/main.dart
+++ lib/main.dart
@@ -26,25 +26,28 @@ class _RelaxState extends State<Relax> {
 
   late Timer timer;
 
-  _StartTimer(){
+  _StartTimer() {
     TimeInMinut = 25;
     int Time = TimeInMinut * 60;
-    double SecPercent = (Time/100);
+    double SecPercent = (Time / 100);
     timer = Timer.periodic(Duration(seconds: 1), (timer) {
       setState(() {
-        if(Time > 0){
+        if (Time > 0) {
           Time--;
-          if(Time % 60 == 0){ // a minute has passed already
+          if (Time % 60 == 0) {
+            // a minute has passed already
             TimeInMinut--;
-          } if(Time % SecPercent == 0){  // 1% = to progress bar
-            if(percent < 1){
+          }
+          if (Time % SecPercent == 0) {
+            // 1% = to progress bar
+            if (percent < 1) {
               percent += 0.01;
-            }else{
+            } else {
               percent = 1;
             }
           }
-        }else{
-          percent= 0;
+        } else {
+          percent = 0;
           TimeInMinut = 25;
           timer.cancel();
         }
@@ -73,7 +76,7 @@ class _RelaxState extends State<Relax> {
                                 color: Colors.white, fontSize: 40.0))),
                     Expanded(
                       child: CircularPercentIndicator(
-                        circularStrokeCap: CircularStrokeCap.round,
+                          circularStrokeCap: CircularStrokeCap.round,
                           percent: percent,
                           animation: true,
                           animateFromLastPercent: true,
@@ -82,96 +85,69 @@ class _RelaxState extends State<Relax> {
                           progressColor: Colors.white,
                           center: Text("$TimeInMinut",
                               style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 80.0
-                              )
-                          )
-                      ),
+                                  color: Colors.white, fontSize: 80.0))),
                     ),
-                    SizedBox(height: 30.0,),
+                    SizedBox(
+                      height: 30.0,
+                    ),
                     Expanded(
-                    child: Container(
-                    width: double.infinity,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(topRight: Radius.circular(30.0),topLeft: Radius.circular(30.0))
-                      ),
-                      child: Padding(
-                      padding: EdgeInsets.only(top: 30.0, left: 20.0, right: 20.0),
-                        child: Column(
-                        children: <Widget>[
-                          Expanded(
-                            child: Row(
-                              children:<Widget>[
+                        child: Container(
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.only(
+                                    topRight: Radius.circular(30.0),
+                                    topLeft: Radius.circular(30.0))),
+                            child: Padding(
+                              padding: EdgeInsets.only(
+                                  top: 30.0, left: 20.0, right: 20.0),
+                              child: Column(children: <Widget>[
                                 Expanded(
-                                  child: Column(
-                                    children: <Widget>[
-                                      Text(
-                                        "Study Timer",
-                                        style: TextStyle(
-                                            fontSize: 30.0
-                                        ),
-                                      ),
-                                      SizedBox(height:10.0,),
-                                      Text(
-                                          "25",
-                                          style: TextStyle(
-                                              fontSize: 80.0
-                                          )
-                                      )
-                                    ]
-                                  )
-                                ),
-                                Expanded(
-                                    child: Column(
-                                        children: <Widget>[
+                                  child: Row(children: <Widget>[
+                                    Expanded(
+                                        child: Column(children: <Widget>[
+                                          Text(
+                                            "Study Timer",
+                                            style: TextStyle(fontSize: 30.0),
+                                          ),
+                                          SizedBox(
+                                            height: 10.0,
+                                          ),
+                                          Text("25",
+                                              style: TextStyle(fontSize: 80.0))
+                                        ])),
+                                    Expanded(
+                                        child: Column(children: <Widget>[
                                           Text(
                                             "Pause Timer",
-                                            style: TextStyle(
-                                                fontSize: 30.0
-                                            ),
+                                            style: TextStyle(fontSize: 30.0),
                                           ),
-                                          SizedBox(height:10.0,),
-                                          Text(
-                                              "5",
-                                              style: TextStyle(
-                                                  fontSize: 80.0
-                                              )
-                                          )
-                                        ]
-                                    )
+                                          SizedBox(
+                                            height: 10.0,
+                                          ),
+                                          Text("5",
+                                              style: TextStyle(fontSize: 80.0))
+                                        ])),
+                                  ]),
                                 ),
-                              ]
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.symmetric(vertical: 28.0),
-                            child: RaisedButton(
-                              onPressed: _StartTimer,
-                              color: Colors.blue,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(100.0)
-                              ),
-                              child: Padding(
-                                padding: EdgeInsets.all(20.0),
-                                child: Text("Start to Study",
-                                style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 22.0
-                                )
-                                )
-                              )
-                            )
-                          )
-                        ]
-                        ),
-                      )
-                    )
-                    ),
-                  ]
-              )
-          )
-      ),
+                                Padding(
+                                    padding:
+                                    EdgeInsets.symmetric(vertical: 28.0),
+                                    child: RaisedButton(
+                                        onPressed: _StartTimer,
+                                        color: Colors.blue,
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                            BorderRadius.circular(100.0)),
+                                        child: Padding(
+                                            padding: EdgeInsets.all(20.0),
+                                            child: Text("Start to Study",
+                                                style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 22.0)))))
+                              ]),
+                            ))),
+                  ]))),
     );
   }
 }
